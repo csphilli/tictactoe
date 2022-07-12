209d366e84159617d68b82cd7f857163ac884620
Christopher Phillips <csphilli@gmail.com>
2022-07-12 12:57:03 +0300

    added readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Tic Tac Go
+
+Basic command line game of tic tac toe built in Go
+
+## To Use
+
+**Method 1**
+
+`go run main.go map.go play.go start.go`
+
+**Method 2**
+
+`go build main.go map.go play.go start.go` and then run the executable.
--- start.go
+++ start.go
@@ -8,7 +8,7 @@ func welcomeText() {
 	fmt.Println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 	fmt.Println("This is a 2 player game.")
-	fmt.Println("Player 1 is 'X' and will go first.\n")
+	fmt.Println("Player 1 is 'X' and will go first.")
 	fmt.Println("Players choose their moves by entering the corresponding grid number.")
 	fmt.Println("Each new turn will re-populate the board")
 
